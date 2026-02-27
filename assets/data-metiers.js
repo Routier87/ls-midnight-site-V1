8859d691234e7da446cc2e07fd1c297c82092bea
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-02-28 00:04:48 +0100

    Add LS_METIERS data structure for roles

---
+++ assets/data-metiers.js
@@ -0,0 +1,20 @@
+window.LS_METIERS = [
+  {
+    id: "lscustom",
+    name: "LS Custom",
+    description: "Mécanique, réparations, customisation (selon vos règles RP).",
+    discordRoleId: "A_METTRE_PLUS_TARD",
+    members: [
+      { name: "Synchronisation Discord plus tard" }
+    ]
+  },
+  {
+    id: "concessionnaire",
+    name: "Concessionnaire",
+    description: "Vente de véhicules (selon vos règles RP).",
+    discordRoleId: "A_METTRE_PLUS_TARD",
+    members: [
+      { name: "Synchronisation Discord plus tard" }
+    ]
+  }
+];
