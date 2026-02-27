e24fffc8ec5b14142fd7f1a76b2cbd3ccd619164
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-02-27 23:55:55 +0100

    Create data-musiques.js with initial playlist
    Added initial music playlist data structure.

---
+++ assets/data-musiques.js
@@ -0,0 +1,7 @@
+window.LS_MUSIQUES = [
+  {
+    title: "LS Midnight - Playlist (YouTube) [Exemple]",
+    embedUrl: "https://www.youtube.com/embed/videoseries?list=PLxxxxxx"
+  }
+  // Ajoute tes playlists ici (YouTube/SoundCloud/Spotify embed)
+];
