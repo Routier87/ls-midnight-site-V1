4c7885b5139800633d04f5532c787ed8076c3a5c
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-03-05 19:18:37 +0100

    Create catalogue.js

---
+++ assets/catalogue.js
@@ -0,0 +1,27 @@
+// Catalogue V1 (tu peux modifier les véhicules quand tu veux)
+window.LS_CATALOGUE = {
+  voiture: [
+    { name:"Sultan RS", brand:"Karin", price:"85 000$", tag:"Sport", stock:"Disponible" },
+    { name:"Buffalo STX", brand:"Bravado", price:"120 000$", tag:"Muscle", stock:"Disponible" },
+    { name:"Schafter V12", brand:"Benefactor", price:"150 000$", tag:"Luxury", stock:"Sur commande" },
+    { name:"Banshee 900R", brand:"Bravado", price:"220 000$", tag:"Super", stock:"Disponible" }
+  ],
+  moto: [
+    { name:"Bati 801", brand:"Pegassi", price:"35 000$", tag:"Sport", stock:"Disponible" },
+    { name:"Sanchez", brand:"Maibatsu", price:"22 000$", tag:"Offroad", stock:"Disponible" },
+    { name:"Nightblade", brand:"Western", price:"40 000$", tag:"Custom", stock:"Sur commande" },
+    { name:"Akuma", brand:"Dinka", price:"45 000$", tag:"Sport", stock:"Disponible" }
+  ],
+  bateau: [
+    { name:"Dinghy", brand:"Nagasaki", price:"55 000$", tag:"Mer", stock:"Disponible" },
+    { name:"Speeder", brand:"Pegassi", price:"120 000$", tag:"Rapide", stock:"Sur commande" },
+    { name:"Squalo", brand:"Shitzu", price:"95 000$", tag:"Mer", stock:"Disponible" },
+    { name:"Jetmax", brand:"Shitzu", price:"140 000$", tag:"Rapide", stock:"Disponible" }
+  ],
+  aeroport: [
+    { name:"Frogger", brand:"Buckingham", price:"450 000$", tag:"Hélico", stock:"Sur commande" },
+    { name:"Maverick", brand:"Buckingham", price:"520 000$", tag:"Hélico", stock:"Disponible" },
+    { name:"Velum", brand:"JoBuilt", price:"380 000$", tag:"Avion", stock:"Disponible" },
+    { name:"Luxor", brand:"Buckingham", price:"1 200 000$", tag:"Jet", stock:"Sur commande" }
+  ]
+};
